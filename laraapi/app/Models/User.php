c4e54df0ac62dea89c71596d0e10da7b49b7cc54
Abdou-4000 <abderrahman.arreffag@cursist.syntrapxl.be>
2025-01-20 19:27:29 +0100

    rooting updated

--- laraapi/app/Models/User.php
+++ laraapi/app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+//use Laravel\Scout\Searchable;
 
 class User extends Authenticatable
 {
