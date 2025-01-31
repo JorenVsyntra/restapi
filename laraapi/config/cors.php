9fce1aa46af803c519fbe7823531a2a6eb99383b
Abdou-4000 <abderrahman.arreffag@cursist.syntrapxl.be>
2025-01-31 20:43:40 +0100

    update

--- laraapi/config/cors.php
+++ laraapi/config/cors.php
@@ -2,8 +2,8 @@
 return [
 'paths'  => ['*'],
 'allowed_methods'  => ['*'],
-'allowed_origins'  => ['*'],
+'allowed_origins'  => ['http://localhost:4200'],
 'allowed_headers'  => ['*'],
 'exposed_headers'  => [],
-'supports_credentials'  =>  false,
+'supports_credentials'  =>  true,
 ];
